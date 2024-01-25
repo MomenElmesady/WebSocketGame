c43d27234e498e2beb1b8f4ae929f3f5113344b2
Momen Ashraf <momenelmesady4803@gmail.com>
2024-01-25 16:23:22 +0200

    style

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require("http")
 const app = require("express")()
-app.listen(9091,()=>console.log("app listining on 9091"))
-app.get("/",(req,res)=> res.sendFile(__dirname+"/index.html"))
+app.listen(9091, () => console.log("app listining on 9091"))
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
 const websocketServer = require("websocket").server
 const server = http.createServer()
 
 
-server.listen(9090,()=>{
+server.listen(9090, () => {
   console.log("listening")
 })
 
@@ -16,47 +16,47 @@ const webServer = new websocketServer({
   "httpServer": server
 })
 
-webServer.on("request",request => {
-  const connection = request.accept(null,request.origin)
-  connection.on("open",()=> console.log('opend'))
-  connection.on("close",()=> console.log('opend'))
-  connection.on("message",message=> {
+webServer.on("request", request => {
+  const connection = request.accept(null, request.origin)
+  connection.on("open", () => console.log('opend'))
+  connection.on("close", () => console.log('opend'))
+  connection.on("message", message => {
     const result = JSON.parse(message.utf8Data)
 
     // user create new game
-    if (result.method === "create"){
+    if (result.method === "create") {
       const clientId = result.clientId
       const gameId = guid()
       games[gameId] = {
         "id": gameId,
-        "balls":15,
-        "clients":[]
+        "balls": 15,
+        "clients": []
       }
       const payLoad = {
         method: "create",
         "game": games[gameId]
       }
 
-      for (let client of Object.keys(clients)){
+      for (let client of Object.keys(clients)) {
         const con = clients[client].connection
         con.send(JSON.stringify(payLoad))
       }
     }
 
-    if (result.method === "join"){
+    if (result.method === "join") {
       const clientId = result.clientId
       const gameId = result.gameId
       const game = games[gameId]
-      if (game.clients.length >=2){
-        return 
+      if (game.clients.length >= 2) {
+        return 0
       }
-      const color = {"0":"Red","1":"blue"}[game.clients.length]
+      const color = { "0": "Red", "1": "blue" }[game.clients.length]
       game.clients.push({
         clientId,
         color
       })
 
-      if (game.clients.length === 2) updateGameState() 
+      if (game.clients.length === 2) updateGameState()
       const payLoad = {
         "method": "join",
         game
@@ -66,25 +66,25 @@ webServer.on("request",request => {
       })
     }
 
-    if (result.method === "play" ){
+    if (result.method === "play") {
       const gameId = result.gameId
       const game = games[gameId]
-      const ballId = result.ballId 
+      const ballId = result.ballId
       const color = result.color
-      let state = games[gameId].state 
-      if(!state){
-        state = {} 
+      let state = games[gameId].state
+      if (!state) {
+        state = {}
       }
-      state[ballId] = color 
+      state[ballId] = color
       games[gameId].state = state
       winner = chickWinner(state)
-      if (winner){
+      if (winner) {
         console.log(winner)
         game.clients.forEach(c => {
           if (c.color === winner)
-            clients[c.clientId].connection.send(JSON.stringify({"method": "end", "status": "win"}))
-          else 
-            clients[c.clientId].connection.send(JSON.stringify({"method": "end", "status": "lose"}))
+            clients[c.clientId].connection.send(JSON.stringify({ "method": "end", "status": "win" }))
+          else
+            clients[c.clientId].connection.send(JSON.stringify({ "method": "end", "status": "lose" }))
         })
       }
 
@@ -106,37 +106,37 @@ webServer.on("request",request => {
 
 })
 
-function chickWinner(state){
+function chickWinner(state) {
   let red = 0
   let blue = 0
-  for (let i of Object.keys(state)){
-    if (state[i] === "Red") red+=1 
-    if (state[i] === "blue") blue+=1 
+  for (let i of Object.keys(state)) {
+    if (state[i] === "Red") red += 1
+    if (state[i] === "blue") blue += 1
   }
-  console.log("red" +red)
-  console.log("blue"+blue)
-  if (red+blue == 15){
+  console.log("red" + red)
+  console.log("blue" + blue)
+  if (red + blue == 15) {
     if (blue > red) return "blue"
     return "red"
   }
   return false
 }
 
-function updateGameState(){
-  for (const g of Object.keys(games)){
+function updateGameState() {
+  for (const g of Object.keys(games)) {
     const game = games[g]
     const payLoad = {
       "method": "update",
       game
     }
-    game.clients.forEach(c=>{
+    game.clients.forEach(c => {
       clients[c.clientId].connection.send(JSON.stringify(payLoad))
     })
 
-    setTimeout(updateGameState,500)
+    setTimeout(updateGameState, 500)
   }
 }
-function S4(){
-  return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+function S4() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 }
-const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
